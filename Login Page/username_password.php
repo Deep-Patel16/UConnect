e4dd8e980e60f10cb24a0f1f0994549b9388f4b5
John <john.deaniii@student.manchester.ac.uk>
2022-03-10 21:18:01 +0000

    first try part hopley done

--- Login Page/username_password.php
+++ Login Page/username_password.php
@@ -1,19 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Details</title>
-</head>
-<body>
 
 
 
-<!--fsd-->
+
 
 
 <?php
 
 require_once('../config.inc.php');
+
+
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
@@ -22,8 +17,12 @@ if($mysqli -> connect_error) {
     die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
 }
 
+$name = $_GET['username'];
+$email = $_GET['email'];
+$password = $_GET['password'];
+
 $sql = "INSERT INTO Users (username, password, id)
-        VALUES ('SteveJobs', 'pass123', 'stevejobs@mircosoft.com')";
+        VALUES ($name, $password, $email)";
 
 if($mysqli->query($sql) === TRUE) {
   echo ("Record added successfully");
@@ -34,7 +33,3 @@ if($mysqli->query($sql) === TRUE) {
 $mysqli->close();
 
 ?>
-
-
-</body>
-</html>
