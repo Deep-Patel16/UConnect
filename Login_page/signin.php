a3e8955db446ee7ecb27467a92343d2e12c51c21
e35918rs <ryan.sullivan-4@student.manchester.ac.uk>
2022-03-18 19:44:46 +0000

    Made folder names url friendly

---
+++ Login_page/signin.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+?>
+<?php
+
+	require_once('../config.inc.php');
+
+	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+	$mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+
+	if($mysqli -> connect_error) {
+    	die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
+	}
+
+	$email = $_GET['email'];
+	$_SESSION['id'] = hash('sha256', $email);
+	$password = $_GET['password'];
+
+	$stmt = $mysqli->prepare("SELECT password FROM Users WHERE id=?");
+	$stmt->bind_param('s', $_SESSION['id']);
+	$stmt->execute();
+	$stmt->bind_result($value);
+	$stmt->fetch();
+
+
+	if ((is_null($value)) || ($value != hash('sha256',$password))) {
+		echo("Incorrect password");
+	} else {
+		echo("Correct password");
+	}
