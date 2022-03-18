b1c201c5d89a08843c724714b90352dbac959129
e35918rs <ryan.sullivan-4@student.manchester.ac.uk>
2022-03-18 00:24:13 +0000

    Initial sign in code - testing functionality

---
+++ Login Page/signin.php
@@ -0,0 +1,23 @@
+<?php
+
+	require_once('../config.inc.php');
+
+	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+	$mysqli = new mysqli($database_host, $database_user, $database_pass, $group_dbnames[0]);
+
+	if($mysqli -> connect_error) {
+    	die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
+	}
+
+	$id = $_GET['email']
+	$password = $_GET['password']
+
+	$stmt = $mysqli->query("SELECT password FROM Users WHERE email=?");
+	$stmt->bind_param('s', $id);
+	$value = $stmt->execute();
+
+	if ((is_null($value)) || ($value == $password)) {
+		echo("Incorrect password");
+	} else {
+		echo("Correct password");
+	}
