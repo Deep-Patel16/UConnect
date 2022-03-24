0abee9ff9378f842278d11b5da9dbc1984a29f4b
e35918rs <ryan.sullivan-4@student.manchester.ac.uk>
2022-03-24 18:08:39 +0000

    Changed titles of webpages

--- Home_page/index.php
+++ Home_page/index.php
@@ -5,7 +5,7 @@ session_start();
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - Pure CSS Gradient Background Animation</title>
+  <title>Home</title>
   <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 <link rel='stylesheet' href='https://codepen.io/P1N2O/pen/xxbjYqx.css'><link rel="stylesheet" href="./style.css">
 
--- Profile_page/profilepage.php
+++ Profile_page/profilepage.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <title>Profile Page</title>
+    <title>Profile</title>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css' rel='stylesheet'>
     <script type='text/Javascript'></script>
     <link href='profilepage.css' rel='stylesheet'>
