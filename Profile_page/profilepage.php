ab3ec0b24e0bace976b6d6e605e3a37dc2b70173
j86153jd <john.deaniii@student.manchester.ac.uk>
2022-03-29 11:55:29 +0100

    fixing mistakes 3

--- Profile_page/profilepage.php
+++ Profile_page/profilepage.php
@@ -14,19 +14,18 @@ $dropdown_statement=$mysqli->prepare("SELECT id,Sports,Movies,Books,VideoGames,M
 $dropdown_statement->bind_param("s", $id_var);
 $dropdown_statement->execute();
 $dropdown_statement->bind_result($drop);
+$drop1_array = array(null,null,null,null,null,null,null,null,null,null,null);
+$drop2_array = array(null,null,null,null,null,null,null,null,null,null,null);
+$drop3_array = array(null,null,null,null,null,null,null,null,null,null,null)
 if (is_null($drop)){
-  $drop1_array = array(null,null,null,null,null,null,null,null,null,null,null);
-  $drop2_array = array(null,null,null,null,null,null,null,null,null,null,null);
-  $drop3_array = array(null,null,null,null,null,null,null,null,null,null,null);
+  
 } else {
   $drop_array = $drop ->fetch_array();
 
   //imma just assume that worked perfectly and $drop_array is now an array of the contents of the users intres row or null if the user hasnt filled it in yet
 
   // right so this bit takes the row an reads through it and updates the array for the dropdown to say this is the boi previously selected
-  $drop1_array = array(null,null,null,null,null,null,null,null,null,null,null);
-  $drop2_array = array(null,null,null,null,null,null,null,null,null,null,null);
-  $drop3_array = array(null,null,null,null,null,null,null,null,null,null,null);
+
 
   for ($iter = 1; $iter <= 11; $iter++) {
     switch $drop_array[$iter]{
