b9cb136774c9891690b444a0a5af4450f59e53e5
John <john.deaniii@student.manchester.ac.uk>
2022-03-31 16:15:00 +0100

    take4 repalce snap

--- Friends_page/final.php
+++ Friends_page/final.php
@@ -66,14 +66,14 @@
                 <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block" >
                   <li><a href="../Home_page/index.html"><span style="color:purple">HomePage</span></a></li>
                   <li><a href="../Profile_page/profile.php"><span style="color:purple">Profile</span></a></li>
-                  
+
                   <li><a href="../Login_page/index.html"><span style="color:purple">LogOut</span></a></li>
                 </ul>
               </nav>
             </div>
 
 
-            <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+            <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
 
             </div>
 
