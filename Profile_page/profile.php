2f95782049724979f69597ec3c28702e0a020657
e35918rs <ryan.sullivan-4@student.manchester.ac.uk>
2022-03-31 16:01:12 +0100

    Maybe fixed

--- Profile_page/profile.php
+++ Profile_page/profile.php
@@ -501,7 +501,7 @@ for ($x = 1; $x < 4; $x++) {
     <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js'></script>
     <script type='text/javascript'>
     function hideOld() {
-      document.getElementsByClassName('img-to-hide').style.display = none;
+      document.getElementsByClassName('img-to-hide')[0].style.display = none;
       alert("Woop");
     }
     </script>
